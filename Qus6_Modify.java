7af7e7d9e1e999be34d3b30070a7caa5e1e92780
agb9380 <72142978+agb9380@users.noreply.github.com>
2021-03-12 10:33:00 +0900

    Update Qus6_Modify.java

--- Qus6_Modify.java
+++ Qus6_Modify.java
@@ -9,7 +9,7 @@ public class Qus6_Modify {
 		
 		
 		
-		System.out.println("10개의 정수를 입력하세요 :");
+		System.out.println("10개의 정수를 입력하세요 :");   // 배열을 이용해서 수정했음
 		Scanner sc = new Scanner(System.in);
 		int [] Arr = new int[10];
 		
@@ -17,17 +17,6 @@ public class Qus6_Modify {
 			Arr[i] = sc.nextInt();
 		}
 		
-		/*
-		 * Scanner sc = new Scanner(System.in); System.out.println("첫 번째 정수를 입력하세요.");
-		 * int num1 = sc.nextInt(); System.out.println("두 번째 정수를 입력하세요."); int num2 =
-		 * sc.nextInt(); System.out.println("세 번째 정수를 입력하세요."); int num3 = sc.nextInt();
-		 * System.out.println("네 번째 정수를 입력하세요."); int num4 = sc.nextInt();
-		 * System.out.println("다섯 번째 정수를 입력하세요."); int num5 = sc.nextInt();
-		 * System.out.println("여섯 번째 정수를 입력하세요."); int num6 = sc.nextInt();
-		 * System.out.println("일곱 번째 정수를 입력하세요."); int num7 = sc.nextInt();
-		 * System.out.println("여덟 번째 정수를 입력하세요."); int num8 = sc.nextInt();
-		 * System.out.println("아홉 번째 정수를 입력하세요.."); int num9 = sc.nextInt();
-		 */
 
 		int sum1 = 0;
 		int sum2 = 0;
@@ -59,32 +48,3 @@ public class Qus6_Modify {
 	}
 }
 
-/*
-	종범씨...
-		Scanner sc = new Scanner(System.in);
-		System.out.println("첫 번째 정수를 입력하세요.");
-		int num1 = sc.nextInt();
-		System.out.println("두 번째 정수를 입력하세요.");
-		int num2 = sc.nextInt();
-		System.out.println("세 번째 정수를 입력하세요.");
-		int num3 = sc.nextInt();
-		System.out.println("네 번째 정수를 입력하세요.");
-		int num4 = sc.nextInt();
-		System.out.println("다섯 번째 정수를 입력하세요.");
-		int num5 = sc.nextInt();
-		System.out.println("여섯 번째 정수를 입력하세요.");
-		int num6 = sc.nextInt();
-		System.out.println("일곱 번째 정수를 입력하세요.");
-		int num7 = sc.nextInt();
-		System.out.println("여덟 번째 정수를 입력하세요.");
-		int num8 = sc.nextInt();
-		System.out.println("아홉 번째 정수를 입력하세요..");
-		int num9 = sc.nextInt();
-
-	이 코드를 짜면서 욱했을거 같은데요??? ^^
-	배열을 이용해서 입력받으면.... 
-	int[] arr = { num1, num2, num3, num4, num5, num6, num7, num8, num9 };
-
-	이런 초기화도 구성할 필요가 없습니다.
-	다시 한번 해보세요~~
-*/
