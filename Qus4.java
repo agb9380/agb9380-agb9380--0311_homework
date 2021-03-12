b345e2ec8f0cd5399716a3c1ce7b4d6f8ec1c544
agb9380 <72142978+agb9380@users.noreply.github.com>
2021-03-12 10:01:05 +0900

    Update Qus4.java

--- Qus4.java
+++ Qus4.java
@@ -10,11 +10,12 @@ public class Qus4 {
 
 		System.out.println("정수를 입력하세요 :");
 		int num = sc.nextInt();
-		boolean search= true;
+		boolean search= true; // 초기화를 true, false 무엇으로 하든 상관없음, true라고하면 소수라고 가정
 		
 		for (int i = 2; i < num; i++) {
 			if (num % i == 0) {
-				search = false;
+				search = false; // 약수가 존재하는지 안하는지에 대한 판단을 하고 싶은 경우는 boolean 형을 많이 사용함
+				break;
 			}
 		}
 		if (search == true) {
